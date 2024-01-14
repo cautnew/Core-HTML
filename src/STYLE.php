3acbe1009d32bad69eb0a08d930e33cef213b2df
Felipe Martins <felipedesmartins@gmail.com>
2024-01-13 21:52:32 -0300

    Created TAG STYLE

---
+++ src/STYLE.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cautnew\HTML;
+
+use Cautnew\HTML\TAG;
+
+class STYLE extends TAG
+{
+  public function __construct(?string $src = null, ... $attrList)
+  {
+    $this->setTagName('style');
+
+    $this->setAttr('src', $src);
+
+    if ($attrList !== null) {
+      $this->setAttributeList($attrList);
+    }
+  }
+}
